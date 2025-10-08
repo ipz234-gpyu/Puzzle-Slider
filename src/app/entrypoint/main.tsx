b8b2655f1ba50619eccab74f99523eb278eefdba
ipz234-gpyu <ipz234_gpyu@student.ztu.edu.ua>
2025-10-08 14:15:33 +0300

    [FEATURES] Structural changes - Change of some directory names - Creation of the structure of the initial pages

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './app/providers/App.tsx'
+import '../styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+
   </StrictMode>,
 )

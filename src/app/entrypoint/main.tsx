3f3f6a4ab8059624d8561ac89400227c906a23b3
ipz234-gpyu <ipz234_gpyu@student.ztu.edu.ua>
2025-10-08 15:03:52 +0300

    [FEATURES] Basic states—placeholders on the main page

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import '../styles/index.css'
+import {MainPage} from '@/pages/main'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-
-  </StrictMode>,
+    <StrictMode>
+        <MainPage/>
+    </StrictMode>,
 )
